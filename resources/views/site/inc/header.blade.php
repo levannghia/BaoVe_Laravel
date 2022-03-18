54c78b3273bfbd6fc726e23be16b7261da2e6f7e
TranThiHoaiTrang <68778348+TranThiHoaiTrang@users.noreply.github.com>
2022-03-18 16:59:20 +0700

    update

--- resources/views/site/inc/header.blade.php
+++ resources/views/site/inc/header.blade.php
@@ -97,9 +97,15 @@
                         <li class="line"></li>
                     </ul>
                     <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2 search-input" type="search" placeholder="Nhập từ khóa tìm kiếm..." aria-label="Search">
-                        <button style="    border-radius: 9px; padding: 3px;" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <!-- <input class="form-control mr-sm-2 search-input" type="search" placeholder="Nhập từ khóa tìm kiếm..." aria-label="Search">
+                        <button style="    border-radius: 9px; padding: 3px;" class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button> -->
+                    
+                        <div id="search">
+                            <input type="text" name="keyword" id="keyword" class="form-control mr-sm-2 search-input" type="search" placeholder="Nhập từ khóa tìm kiếm..." aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true" ></i></button>
+                        </div>
                     </form>
+                    
                 </div>
             </nav>
         </div>
